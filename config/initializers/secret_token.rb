ebc2b747944390f4354db0bb7c1401cb0cde0f06
Marko Matijevic <elektronski.matijevic@gmail.com>
2011-11-08 20:19:59 +0100

    Inicijalni commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skillstore::Application.config.secret_token = '68b96fc5b453aa67d56643f895adc309fcf2cdbb2e59d7bb4a197aabc29c602ad14b82581c94739b204cf75a208df8af271be57abc042abb299fc334b1c05f8b'
